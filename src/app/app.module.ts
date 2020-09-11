b70bb05f0e02d3a7efcb4405e6df54b0fea9aa0a
Oleksandr Nikolskyi <nikolskiy90@gmail.com>
2020-09-11 21:43:06 +0300

    create cv for hys

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoverLetterModule } from './components/cover-letter/cover-letter.module';
+import { CvModule } from './components/cv/cv.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CoverLetterModule,
+    CvModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
